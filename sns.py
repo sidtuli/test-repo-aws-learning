c90cfd0a862f2a0f293d01dc78793dfed231727d
sidtuli <sidtuli@users.noreply.github.com>
2020-02-26 20:42:55 -0600

    Update sns.py

--- sns.py
+++ sns.py
@@ -4,8 +4,8 @@ import boto3
 def send_sns_message ():
     sns = boto3.client('sns')
     sns.publish(
-        TopicArn="arn:aws:sns:us-west-1:361310239170:hello-there",    
-        Message="hello"
+        TopicArn="<SNS-ARN-HERE>",    
+        Message="<message>"
     )
 
 def lambda_handler(event, context):

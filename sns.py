bb9b6e4aa7b3490cde312a2315d36ad5770609c7
sidtuli <sidtuli@users.noreply.github.com>
2020-02-26 20:32:11 -0600

    Create sns.py

---
+++ sns.py
@@ -0,0 +1,14 @@
+import boto3
+
+# Function to send message to random SNS topic
+def send_sns_message ():
+    sns = boto3.client('sns')
+    sns.publish(
+        TopicArn="arn:aws:sns:us-west-1:361310239170:hello-there",    
+        Message="hello"
+    )
+
+def lambda_handler(event, context):
+    # TODO implement
+    send_sns_message()
+    
